b6c8dff2fac0e02fc12483e5d95340524564ec05
Jason Welch <jasoncancode@gmail.com>
2020-11-03 17:37:07 -0500

    WIP

--- PhotoBrowserExample/PhotoBrowserViewController.swift
+++ PhotoBrowserExample/PhotoBrowserViewController.swift
@@ -7,20 +7,44 @@
 
 import UIKit
 
+private enum BrowserMode {
+    case paging
+    case zoom
+}
+
 class PhotoBrowserViewController: UIViewController {
-    @IBOutlet weak private var navBar: UINavigationBar!
+    @IBOutlet weak private var navBar: UINavigationItem!
     @IBOutlet weak private(set) var scrollView: UIScrollView!
-    @IBOutlet weak private var containerView: UIView!
     @IBOutlet weak private var bottomToolbar: UIToolbar!
 
     private let content: [PhotoPageContentRepresentable]
     private let pagingViewController: PhotoPagingViewController
-    private var currentPageIndex = 0
+    private var currentPageIndex = 0 {
+        didSet {
+            updateTitle()
+        }
+    }
+    private var mode: BrowserMode = .zoom
+    
+    private let photoViews: [AsyncImageView]
+    private var currentPhotoView: AsyncImageView? {
+        guard currentPageIndex < photoViews.count else {
+            return nil
+        }
+        return photoViews[currentPageIndex]
+    }
 
     init(content: [PhotoPageContentRepresentable]) {
         self.content = content
         self.pagingViewController = PhotoPagingViewController()
         self.pagingViewController.content = content
+        
+        self.photoViews = content.map {
+            let view = AsyncImageView()
+            view.updateImage(fromURLString: $0.imagePath, placeholderImage: $0.placeholderImage)
+            view.contentMode = .scaleAspectFit
+            return view
+        }
 
         super.init(nibName: "PhotoBrowserViewController", bundle: nil)
     }
@@ -32,16 +56,65 @@ class PhotoBrowserViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        let childVC = pagingViewController
-        addChild(childVC)
-        //Or, you could add auto layout constraint instead of relying on AutoResizing contraints
-        childVC.view.autoresizingMask = [.flexibleHeight, .flexibleWidth]
-        childVC.view.frame = containerView.bounds
-
-        containerView.addSubview(childVC.view)
-        childVC.didMove(toParent: self)
+        scrollView.minimumZoomScale = 1
+        updateTitle()
+        configureZoomMode()
     }
-
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        update(mode: .paging)
+    }
+    
+    private func updateTitle() {
+        navBar.title = "\(currentPageIndex + 1) of \(content.count)"
+    }
+    
+    private func update(mode: BrowserMode) {
+        if self.mode != mode {
+            self.mode = mode
+            
+            switch mode {
+            case .paging:
+                configurePagingMode()
+            case .zoom:
+                configureZoomMode()
+            }
+        }
+    }
+    
+    private func configureZoomMode() {
+        let width = UIScreen.main.bounds.size.width
+        
+        scrollView.frame = CGRect(x: 0, y: 0, width: width, height: view.frame.height)
+        scrollView.contentSize = CGSize(width: width, height: scrollView.frame.height)
+        scrollView.isPagingEnabled = false
+        scrollView.isDirectionalLockEnabled = false
+        
+        scrollView.subviews.forEach {
+            if $0 != currentPhotoView {
+                $0.removeFromSuperview()
+            }
+        }
+        currentPhotoView?.frame = CGRect(x: 0, y: 0, width: width, height: scrollView.frame.height)
+    }
+    
+    private func configurePagingMode() {
+        let width = UIScreen.main.bounds.size.width
+        
+        scrollView.frame = CGRect(x: 0, y: 0, width: width, height: view.frame.height)
+        scrollView.contentSize = CGSize(width: width * CGFloat(photoViews.count), height: scrollView.frame.height)
+        scrollView.isPagingEnabled = true
+        scrollView.isDirectionalLockEnabled = true
+        
+        for (i, imageView) in photoViews.enumerated() {
+            imageView.frame = CGRect(x: width * CGFloat(i), y: 0, width: width, height: scrollView.frame.height)
+            scrollView.addSubview(imageView)
+        }
+        
+        scrollView.contentOffset = CGPoint(x: width * CGFloat(currentPageIndex), y: 0)
+    }
 
     @IBAction private func close() {
         dismiss(animated: true, completion: nil)
@@ -49,30 +122,33 @@ class PhotoBrowserViewController: UIViewController {
 
 }
 
-extension PhotoBrowserViewController: PageContainerDelegate {
-    func switchedPage(to index: Int, isLastPage: Bool) {
-        currentPageIndex = index
-        let pageVCs = pagingViewController.orderedViewControllers
-
-        guard index < pageVCs.count else {
-            return
-        }
-        let photoVC = pageVCs[index] as? PhotoViewController
-        photoVC?.scrollView.delegate = self
-    }
-}
-
 // MARK: - Zooming
 
 extension PhotoBrowserViewController: UIScrollViewDelegate {
-
-    func viewForZooming(in scrollView: UIScrollView) -> UIView? {
-        let pageVCs = pagingViewController.orderedViewControllers
-
-        guard currentPageIndex < pageVCs.count else {
-            return nil
+    
+    func scrollViewDidEndDecelerating(_ scrollView: UIScrollView) {
+        let isPagingMode = scrollView.subviews.count > 1
+        
+        if isPagingMode {
+            currentPageIndex = Int(round(scrollView.contentOffset.x / scrollView.frame.width))
         }
-        let photoVC = pageVCs[currentPageIndex] as? PhotoViewController
-        return photoVC?.viewForZooming(in: scrollView)
+    }
+    
+    // MARK: - Zoom
+    
+    func viewForZooming(in scrollView: UIScrollView) -> UIView? {
+        update(mode: .zoom)
+        return currentPhotoView
+    }
+    
+//    func scrollViewWillBeginZooming(_ scrollView: UIScrollView, with view: UIView?) {
+//        update(mode: .zoom)
+//    }
+    
+    func scrollViewDidEndZooming(_ scrollView: UIScrollView, with view: UIView?, atScale scale: CGFloat) {
+        if scale <= 1.25 {
+            scrollView.setZoomScale(1, animated: true)
+            update(mode: .paging)
+        }
     }
 }
--- PhotoBrowserExample/ViewController.swift
+++ PhotoBrowserExample/ViewController.swift
@@ -18,6 +18,8 @@ class ViewController: UIViewController {
         let imagePaths = [
             "https://en.bcdn.biz/Images/2016/11/15/776342f0-86f5-4522-84c9-a02d6b11c766.jpg",
             "https://en.bcdn.biz/Images/2016/11/15/776342f0-86f5-4522-84c9-a02d6b11c766.jpg",
+            "https://en.bcdn.biz/Images/2016/11/15/776342f0-86f5-4522-84c9-a02d6b11c766.jpg",
+            "https://en.bcdn.biz/Images/2016/11/15/776342f0-86f5-4522-84c9-a02d6b11c766.jpg",
             "https://en.bcdn.biz/Images/2016/11/15/776342f0-86f5-4522-84c9-a02d6b11c766.jpg"
         ]
         let content: [PhotoPageContentRepresentable] = imagePaths.map { PhotoPageContent(imagePath: $0) }
