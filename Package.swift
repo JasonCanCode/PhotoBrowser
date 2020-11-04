c59cf67f35c691ce90c61b5dd4c2d9e33542e9b9
Jason Welch <jasoncancode@gmail.com>
2020-11-04 16:04:36 -0500

    Packge reference fix

--- Package.swift
+++ Package.swift
@@ -20,10 +20,9 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "PhotoBrowser",
-            path: "Source"),
+            dependencies: []),
         .testTarget(
             name: "PhotoBrowserTests",
-            dependencies: ["PhotoBrowser"],
-            path: "Tests"),
+            dependencies: ["PhotoBrowser"]),
     ]
 )
